17134bcdfbeaf89e8e4f776ab86f1982bc645169
Raven Burkard <mathematti@outlook.com>
2024-01-29 18:32:24 +0100

    Improvements

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php
+
+//einmal autoload requiren
+require_once 'vendor/autoload.php';
+
 //von https://packagist.org/packages/endroid/qr-code
 use Endroid\QrCode\Builder\Builder;
 use Endroid\QrCode\Encoding\Encoding;
 use Endroid\QrCode\ErrorCorrectionLevel;
 use Endroid\QrCode\Label\LabelAlignment;
-use Endroid\QrCode\Label\Font\NotoSans;
 use Endroid\QrCode\RoundBlockSizeMode;
 use Endroid\QrCode\Writer\PngWriter;
 
-//einmal autoload requiren
-require_once 'vendor/autoload.php';
 
-function generateQrCode($input)
+function generateQrCode($input): \Endroid\QrCode\Writer\Result\ResultInterface
 {
     return
         Builder::create()
@@ -30,86 +31,50 @@ function generateQrCode($input)
             ->build();
 }
 
+
+$htmlStart = <<<HTML
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>QR-Code Generator</title>
+</head>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap')
+</style>
+<body>
+<main>
+<h1 class="title">Generate a QR-Code!</h1>
+<form class="inputForm" method="post">
+<input type="text" name="inputValue" id="inputField" placeholder="text to generate">
+<button type="submit" id="submitButton">generate</button>
+</form>
+HTML;
+
+echo $htmlStart;
+
 // Handle form submission
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["inputValue"])) {
     $inputValue = $_POST["inputValue"];
     $qrCodeDataUrl = 'data:image/png;base64,' . base64_encode(generateQrCode($inputValue)->getString());
     // Return HTML content with QR code inside the original <div>
-    $htmlwithqrcode = <<<HTML
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css">
-    <title>QR-Code Generator</title>
-</head>
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap')
-</style>
-<body>
-<main>
-<h1 class="title">Generate a QR-Code!</h1>
-<form class="inputForm" method="post">
-<input type="text" name="inputValue" id="inputField" placeholder="text to generate">
-<button type="submit" id="submitButton">generate</button>
-</form>
-<div class="qrcode" id="qrcodeContainer">
-    <img src=$qrCodeDataUrl alt="qrcode">
-    <p>QR Code for: $inputValue</p>
-</div>
-</main>
-</body>
-<script>
-document.addEventListener("DOMContentLoaded", function() {
-    let button = document.getElementById("submitButton");
-    let form = document.querySelector(".inputForm");
+    $qrcodeBlock = <<<HTML
+    <div class="qrcode" id="qrcodeContainer">
+        <img src=$qrCodeDataUrl alt="qrcode">
+        <p>QR Code for: $inputValue</p>
+    </div>
+    HTML;
 
-    button.addEventListener("click", function(event) {
-        let inputValue = document.getElementById("inputField").value.trim();
-        if (!inputValue) {
-            event.preventDefault(); // Prevent default form submission if input field is empty
-            alert("Please enter text to generate QR code.");
-        }
-    });
-});
-</script>
-</html>
-HTML;
-
-    echo $htmlwithqrcode;
-
-    exit(); // Stop further execution
+    echo $qrcodeBlock;
 }
 
-
-// Generate HTML output
-$htmlOutput = <<<HTML
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css">
-    <title>QR-Code Generator</title>
-</head>
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap')
-</style>
-<body>
-<main>
-<h1 class="title">Generate a QR-Code!</h1>
-<form class="inputForm" method="post">
-<input type="text" name="inputValue" id="inputField" placeholder="text to generate">
-<button type="submit" id="submitButton">generate</button>
-</form>
-<div class="qrcode" id="qrcodeContainer">
-    <!-- QR Code will be dynamically inserted here -->
-</div>
+$htmlEnd = <<<HTML
 </main>
 </body>
 <script>
 document.addEventListener("DOMContentLoaded", function() {
     let button = document.getElementById("submitButton");
-    let form = document.querySelector(".inputForm");
 
     button.addEventListener("click", function(event) {
         let inputValue = document.getElementById("inputField").value.trim();
@@ -123,4 +88,4 @@ document.addEventListener("DOMContentLoaded", function() {
 </html>
 HTML;
 
-echo $htmlOutput;
+echo $htmlEnd;
